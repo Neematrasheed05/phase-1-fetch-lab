056b8273a52b6863b6b0ab655647dd90cc2e9b30
Neematrashid <nymatrasheed05@gmail.com>
2023-10-01 20:40:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
+    // Make a fetch request to the Game of Thrones API
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(response => {
+        // Check if the response status is OK (200)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        // Convert the response to JSON
+        return response.json();
+      })
+      .then(data => {
+        // Call the renderBooks() function and pass in the JSON data
+        renderBooks(data);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }
   
-}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -9,9 +26,18 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
-}
+      
+    const booksList = document.getElementById('books-list');
+    // Loop through the data and create list items for each book
+      data.forEach(book => {
+        const listItem = document.createElement('li');
+        listItem.textContent = book.name;
+        booksList.appendChild(listItem);
+      });
+    }
+)};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
